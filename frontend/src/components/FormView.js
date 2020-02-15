4e565e3b494534469abaaf1704ab4f13dbe2c383
keingsw <dev.keingsw@gmail.com>
2020-02-15 13:07:11 +0800

    Remove TODO comment

--- frontend/src/components/FormView.js
+++ frontend/src/components/FormView.js
@@ -33,7 +33,7 @@ class FormView extends Component {
   submitQuestion = event => {
     event.preventDefault();
     $.ajax({
-      url: '/questions', //TODO: update request URL
+      url: '/questions',
       type: 'POST',
       dataType: 'json',
       contentType: 'application/json',
